9f9916edd001b3b928d20e209ad36f10b040d89f
Jose Zaccaro <a.zaccaro42@gmail.com>
2024-01-19 14:00:41 -0300

    dia1

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        /* System.out.println("Hello Luigi!"); */
+
+        // Primitivos
+
+        // enteros
+        // byte -> -128 y 127
+        // Byte
+        // short -> -x a x
+        // int -> -x a x
+        // long -> -xL a xL
+
+        // decimales
+        // float -> 3.6f
+//        float decimal = 3.5512412F;
+        // double -> 2.3
+//        double decimal2 = 3.12141121245;
+
+        // booleanos -> boolean
+
+        // char -> caracter -> 'a'
+
+//        byte numeroChiquito = 127;
+//        byte numero2 = 2;
+//
+//
+//        byte suma = numeroChiquito + numero2;
+
+
+        // Wrappers
+
+        // Byte
+//        System.out.println(Byte.MAX_VALUE);
+//        System.out.println(Byte.MIN_VALUE);
+
+        // Short
+//        System.out.println(Short.MAX_VALUE);
+//        System.out.println(Short.MIN_VALUE);
+
+        // Integer
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+
+        // Long
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+
+        // Float
+//        Float decimal = 3.5F;
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Float.MIN_VALUE);
+        // Double
+//        Double decimal2 = 1.1416;
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(Double.MIN_VALUE);
+
+        // Cadenas de texto -> String
+//        String nombre = "Jose";
+//        String nombre2 = "Guille";
+//
+//        String nombres = nombre + nombre2;
+//
+//        String nombre3 = "jOsE";
+
+//            int num = 10;
+//            int num2 = 20;
+
+        // >, <, >=, <=, ==, !=
+
+        // System.out.println(nombre.equalsIgnoreCase(nombre3));
+
+        // Arrays
+        // tipo array que se va a llamar array1 y va a tener tantos elementos como maximo.
+
+//        int[] arrayNumeros = {1,2,3,4,5,6,7,8};
+//        int[] arrayNumeros2 = new int[8];
+//        String[] mentores = {"Jose", "Guille", "Nico","Martin"};
+
+//        for (int i = 0; i < mentores.length; i++) {
+//          System.out.println(mentores[i]);
+//        }
+
+//        for (String nombre : mentores) {
+//            System.out.println(nombre);
+//        }
+
+//        int index = 0;
+//        while(index < arrayNumeros.length){
+//            System.out.println(arrayNumeros[index]);
+//
+//            index++;
+//        }
+//
+//        do {
+//            System.out.println(arrayNumeros[index]);
+//            index++;
+//        }while (index < arrayNumeros.length);
+
+
+//        System.out.println(arrayNumeros2[0]);
+//        Scanner scanner = new Scanner(System.in);
+//
+//
+//        preguntarNombreSaludar();
+//
+//        System.out.println("Ingrese su edad: ");
+//        byte edad = scanner.nextByte();
+//        System.out.println("Su edad es: "+ edad);
+
+
+    }
+
+//    public static void preguntarNombreSaludar(){
+//        System.out.println("Ingrese su nombre: ");
+//        Scanner scanner = new Scanner(System.in);
+//        String nombre = scanner.next();
+//        System.out.println("Hola "+ nombre);
+//    }
+
+
+}
